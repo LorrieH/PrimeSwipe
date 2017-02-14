0261a347de9d3d80a0b05b03f1b91ab12f029946
Niels <Niels97vet@hotmail.com>
2017-02-14 13:26:18 +0100

    Random spawning

---
+++ CandyCrush2/Assets/Scripts/Niels/ShapeSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+
+    private Vector3 _BoxPos;
+    private int _Shape;
+
+    private GameObject _Shape1;
+    private GameObject _Shape2;
+    private GameObject _Shape3;
+
+    private List<GameObject> _Shapes;
+
+    private bool _AllreadyFilled = false;
+
+    // Use this for initialization
+    void Start () {
+        _Shape1 = Resources.Load<GameObject>("Shape1");
+        _Shape2 = Resources.Load<GameObject>("Shape2");
+        _Shape3 = Resources.Load<GameObject>("Shape3");
+
+        _Shapes = new List<GameObject>();
+
+        _Shapes.Add(_Shape1);
+        _Shapes.Add(_Shape2);
+        _Shapes.Add(_Shape3);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_AllreadyFilled == false)
+        {
+            SpawnNew();
+            _AllreadyFilled = true;
+        }
+	}
+
+    private void SpawnNew()
+    {
+        _Shape = Random.Range(0, 3);
+        Instantiate(_Shapes[_Shape], this.transform.position, _Shapes[_Shape].transform.rotation);
+    }
+}
