141a0b20a884873d76680990940f2976d0567803
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-14 14:06:51 +0100

    Swipe Mechanic progress

---
+++ CandyCrush2/Assets/Scripts/SwipeManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeDirection
+{
+    None = 0,
+    Left = 1,
+    Right = 2,
+    Up = 4,
+    Down = 8,
+
+    LeftDown = 9,
+    LeftUp = 5,
+    RightDown = 10,
+    RightUp = 6
+}
+
+public class SwipeManager : MonoBehaviour {
+
+    public SwipeDirection Direction { set; get; }
+
+    private Vector3 _touchPosition;
+    private float _swipeResistanceX = 30;
+    private float _swipeResistanceY = 30;
+
+    private void Update()
+    {
+        Direction = SwipeDirection.None;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 deltaSwipe = _touchPosition - Input.mousePosition;
+
+            if (Mathf.Abs(deltaSwipe.x) > _swipeResistanceX)
+            {
+                //swipe on x axis
+                Direction |= (deltaSwipe.x < 0) ? SwipeDirection.Right : SwipeDirection.Left;
+            }
+            if (Mathf.Abs(deltaSwipe.y) > _swipeResistanceY)
+            {
+                //swipe on y axis
+                Direction |= (deltaSwipe.y < 0) ? SwipeDirection.Up : SwipeDirection.Down;
+            }
+        }
+    }
+
+    public bool IsSwiping(SwipeDirection dir)
+    {
+        return (Direction & dir) == dir;
+    }
+}
---
+++ CandyCrush2/Assets/Scripts/Swiping.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swiping : MonoBehaviour {
+
+    SwipeManager swipeManager;
+
+    void Start()
+    {
+        swipeManager = GetComponent<SwipeManager>();
+    }
+
+	void Update () {
+        if (swipeManager.IsSwiping(SwipeDirection.Up)){
+            Debug.Log("Swipe Up");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.Down))
+        {
+            Debug.Log("Swipe Down");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.Left))
+        {
+            Debug.Log("Swipe Left");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.Right))
+        {
+            Debug.Log("Swipe Right");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.LeftUp))
+        {
+            Debug.Log("Swipe LeftUp");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.RightUp))
+        {
+            Debug.Log("Swipe RightUp");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.LeftDown))
+        {
+            Debug.Log("Swipe LeftDown");
+        }
+        if (swipeManager.IsSwiping(SwipeDirection.RightDown))
+        {
+            Debug.Log("Swipe RightDown");
+        }
+    }
+}
