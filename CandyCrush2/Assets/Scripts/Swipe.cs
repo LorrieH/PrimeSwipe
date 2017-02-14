f2259ffe62b24d22788992faa833cea4b85b0020
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-14 11:04:59 +0100

    Fixed folders

---
+++ CandyCrush2/Assets/Scripts/Swipe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour {
+
+    private Vector3 _touchPosition;
+    private float _swipeResistanceX = 50f;
+    private float _swipeResistanceY = 100f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchPosition = Input.mousePosition;
+        }
+    }
+}
