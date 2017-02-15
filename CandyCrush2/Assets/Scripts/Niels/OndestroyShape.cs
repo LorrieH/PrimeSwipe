59bfa3b517a2272297985da48cfc138991b8f412
Niels <Niels97vet@hotmail.com>
2017-02-15 18:49:49 +0100

    HELP me

---
+++ CandyCrush2/Assets/Scripts/Niels/OndestroyShape.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OndestroyShape : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _anim;
+
+    [SerializeField]
+    private int _waitTime;
+
+    void Awake() {
+        _anim = GetComponent<Animator>();
+    }
+
+    void OnDestroy() {
+        _anim.SetTrigger("death");
+        //StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(_waitTime);
+    }
+}
