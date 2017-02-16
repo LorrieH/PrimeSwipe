6d96279c43bbe174b9b9c105a8283791f822fbf0
Niels <Niels97vet@hotmail.com>
2017-02-16 14:50:43 +0100

    menu

---
+++ CandyCrush2/Assets/Scripts/Niels/MenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour {
+
+    [SerializeField]
+    private int _sceneSelector;
+    [SerializeField]
+    private int _sceneSelector2;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_sceneSelector);
+    }
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(_sceneSelector2);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(_sceneSelector);
+    }
+}
