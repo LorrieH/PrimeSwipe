7f2b839d0b5d67b432248bf5cc451c56cc280492
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-16 15:08:25 +0100

    lorenzo menu build

---
+++ CandyCrush2/Assets/FadePanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadePanel : MonoBehaviour {
+
+    private Animator _anim;
+
+	// Use this for initialization
+	void Start () {
+        _anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SwipeManager.Turns < 1)
+        {
+            _anim.SetBool("GameOver", true);
+        }
+    }
+}
