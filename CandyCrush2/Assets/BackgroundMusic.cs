df64b2ed829f1bcf6d21b4717dff2fada0588eed
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-17 13:01:47 +0100

    perfect game wait for mi art

---
+++ CandyCrush2/Assets/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    private AudioSource _source;
+
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!MuteButton.muted)
+        {
+            _source.volume = 1;
+        }
+        else
+            _source.volume = 0;
+    }
+}
